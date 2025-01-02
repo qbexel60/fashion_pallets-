2a3c3174ac77477f9720e58dccef60940a2e8aa5
SigmaJubayer <act.mosayedul.jubayer@gmail.com>
2025-01-02 23:47:55 +0600

    social lionks

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,8 +1,8 @@
 const socialLinks = {
-  facebook: 'http://www.facebook.com/fashionpallet',
-  instagram: 'http://www.instagram.com/fashionpallet',
-  tiktok: 'http://www.tiktok.com/@fashionpallet',
-  whatsApp: 'http://www.whatsapp.com/fashionpallet',
+  facebook: 'https://www.facebook.com/Fashionpalettebd?mibextid=ZbWKwL',
+  instagram: 'https://www.instagram.com/fashion_pallete_bd?igsh=Ym05M3MwcnJhaWdr',
+  tiktok: 'https://www.tiktok.com/@sumaiyamaliha323?_t=ZS-8sjhK1PQHNI&_r=1',
+  whatsApp: 'https://wa.me/8801943415089?text=Is%20someone%20available%20for%20chat%3F',
 }
 
 export default socialLinks
