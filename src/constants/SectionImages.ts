0301cf5a790f0d96aa04cf15c1777305da972c06
SigmaJubayer <act.mosayedul.jubayer@gmail.com>
2025-01-02 20:51:24 +0600

    real  pics added

--- src/constants/SectionImages.ts
+++ src/constants/SectionImages.ts
@@ -83,7 +83,208 @@ export const sectionImages: {
       name: 'image6',
       url: 'https://i.imgur.com/KzuQKne.jpeg',
     },
+    {
+      name: 'image7',
+      url: 'https://i.imgur.com/VS9zjTA.jpeg',
+    },
+    {
+      name: 'image8',
+      url: 'https://i.imgur.com/H2nFYEP.jpeg',
+    },
+    {
+      name: 'image9',
+      url: 'https://i.imgur.com/riuCqay.jpeg',
+    },
+    {
+      name: 'image10',
+      url: 'https://i.imgur.com/Xe1OUWu.jpeg',
+    },
+    {
+      name: 'image11',
+      url: 'https://i.imgur.com/L5SmN88.jpeg',
+    },
+    {
+      name: 'image12',
+      url: 'https://i.imgur.com/9TDUeYB.jpeg',
+    },
+    {
+      name: 'image13',
+      url: 'https://i.imgur.com/w6sjljN.jpeg',
+    },
+    {
+      name: 'image14',
+      url: 'https://i.imgur.com/xWqYh5I.jpeg',
+    },
+    {
+      name: 'image15',
+      url: 'https://i.imgur.com/ZdlZM2r.jpeg',
+    },
+    {
+      name: 'image16',
+      url: 'https://i.imgur.com/hsgCuMQ.jpeg',
+    },
+    {
+      name: 'image17',
+      url: 'https://i.imgur.com/E0N1nEA.jpeg',
+    },
+    {
+      name: 'image18',
+      url: 'https://i.imgur.com/fJS8Lz1.jpeg',
+    },
+    {
+      name: 'image19',
+      url: 'https://i.imgur.com/1HJpawW.jpeg',
+    },
+    {
+      name: 'image20',
+      url: 'https://i.imgur.com/QO0sq8Y.jpeg',
+    },
+    {
+      name: 'image21',
+      url: 'https://i.imgur.com/xVj2a65.jpeg',
+    },
+    {
+      name: 'image22',
+      url: 'https://i.imgur.com/uzQ7uI3.jpeg',
+    },
+    {
+      name: 'image23',
+      url: 'https://i.imgur.com/JQt2tTH.jpeg',
+    },
+    {
+      name: 'image24',
+      url: 'https://i.imgur.com/tsuT9ai.jpeg',
+    },
+    {
+      name: 'image25',
+      url: 'https://i.imgur.com/QmgUqcT.jpeg',
+    },
+    {
+      name: 'image26',
+      url: 'https://i.imgur.com/twgstHU.jpeg',
+    },
+    {
+      name: 'image27',
+      url: 'https://i.imgur.com/uvXRJ0C.jpeg',
+    },
+    {
+      name: 'image28',
+      url: 'https://i.imgur.com/PoIiV95.jpeg',
+    },
+    {
+      name: 'image29',
+      url: 'https://i.imgur.com/kqZJFJF.jpeg',
+    },
+    {
+      name: 'image30',
+      url: 'https://i.imgur.com/GgcV0E2.jpeg',
+    },
+    {
+      name: 'image31',
+      url: 'https://i.imgur.com/SxHn8pw.jpeg',
+    },
+    {
+      name: 'image32',
+      url: 'https://i.imgur.com/gPc2xH3.jpeg',
+    },
+    {
+      name: 'image33',
+      url: 'https://i.imgur.com/0TLXO8g.jpeg',
+    },
+    {
+      name: 'image34',
+      url: 'https://i.imgur.com/6UXjQQC.jpeg',
+    },
+    {
+      name: 'image35',
+      url: 'https://i.imgur.com/BhBUWrX.jpeg',
+    },
+    {
+      name: 'image36',
+      url: 'https://i.imgur.com/bt10eOs.jpeg',
+    },
+    {
+      name: 'image37',
+      url: 'https://i.imgur.com/JIOmmQB.jpeg',
+    },
+    {
+      name: 'image38',
+      url: 'https://i.imgur.com/cfVz1PB.jpeg',
+    },
+    {
+      name: 'image39',
+      url: 'https://i.imgur.com/DP80QMT.jpeg',
+    },
+    {
+      name: 'image40',
+      url: 'https://i.imgur.com/44X31J2.jpeg',
+    },
+    {
+      name: 'image41',
+      url: 'https://i.imgur.com/b7btwlU.jpeg',
+    },
+    {
+      name: 'image42',
+      url: 'https://i.imgur.com/D6es7fN.jpeg',
+    },
+    {
+      name: 'image43',
+      url: 'https://i.imgur.com/8fKgzGf.jpeg',
+    },
+    {
+      name: 'image44',
+      url: 'https://i.imgur.com/NdfCyQf.jpeg',
+    },
+    {
+      name: 'image45',
+      url: 'https://i.imgur.com/5dvc2n2.jpeg',
+    },
+    {
+      name: 'image46',
+      url: 'https://i.imgur.com/ipzmYpI.jpeg',
+    },
+    {
+      name: 'image47',
+      url: 'https://i.imgur.com/OU6hE7q.jpeg',
+    },
+    {
+      name: 'image48',
+      url: 'https://i.imgur.com/QLZjWgi.jpeg',
+    },
+    {
+      name: 'image49',
+      url: 'https://i.imgur.com/P5TTiAf.jpeg',
+    },
+    {
+      name: 'image50',
+      url: 'https://i.imgur.com/U1XgvNm.jpeg',
+    },
+    {
+      name: 'image51',
+      url: 'https://i.imgur.com/cpd2D7N.jpeg',
+    },
+    {
+      name: 'image52',
+      url: 'https://i.imgur.com/uGUqM6L.jpeg',
+    },
+    {
+      name: 'image53',
+      url: 'https://i.imgur.com/rsr8qk4.jpeg',
+    },
+    {
+      name: 'image54',
+      url: 'https://i.imgur.com/6SQFC73.jpeg',
+    },
+    {
+      name: 'image55',
+      url: 'https://i.imgur.com/D8fwNGV.jpeg',
+    },
+    {
+      name: 'image56',
+      url: 'https://i.imgur.com/Uo5m5h4.jpeg',
+    },
   ],
+
   reviews: [
     {
       name: 'image1',
