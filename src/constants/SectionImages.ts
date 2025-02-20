421f187b3a24e624ba7ef8c98940c49849a93ea7
qbexel60 <qbexel@gmail.com>
2025-02-20 23:20:55 +0600

    real pics 2

--- src/constants/SectionImages.ts
+++ src/constants/SectionImages.ts
@@ -348,88 +348,325 @@ export const sectionImages: {
 
   real: [
     {
-      name: 'image1',
-      url: 'https://i.ibb.co.com/Nns5pNRM/471941142-581408081321413-311395801393515545-n.jpg',
+      name: "image1",
+      url: "https://i.ibb.co.com/PsW4Fjmv/472450463-581884897940398-9167384824013008756-n.jpg",
     },
-    { name: 'image2', url: 'https://ibb.co.com/qYjsm2HS' },
-    { name: 'image3', url: 'https://ibb.co.com/Gv5PM0Hf' },
-    { name: 'image4', url: 'https://ibb.co.com/RTLqS2kW' },
-    { name: 'image5', url: 'https://ibb.co.com/V01jrmhY' },
-    { name: 'image6', url: 'https://ibb.co.com/hxgdQR0w' },
-    { name: 'image7', url: 'https://ibb.co.com/0pqXfY1P' },
-    { name: 'image8', url: 'https://ibb.co.com/9mdq7KpH' },
-    { name: 'image9', url: 'https://ibb.co.com/8gWvk4bX' },
-    { name: 'image10', url: 'https://ibb.co.com/1GJSVDph' },
-    { name: 'image11', url: 'https://ibb.co.com/LdcXgH2P' },
-    { name: 'image12', url: 'https://ibb.co.com/DPcD08PF' },
-    { name: 'image13', url: 'https://ibb.co.com/vCkfFF54' },
-    { name: 'image14', url: 'https://ibb.co.com/pmJJ3zv' },
-    { name: 'image15', url: 'https://ibb.co.com/5hvnQtrT' },
-    { name: 'image16', url: 'https://ibb.co.com/n8rrLt53' },
-    { name: 'image17', url: 'https://ibb.co.com/CpsJx4YW' },
-    { name: 'image18', url: 'https://ibb.co.com/3mBhR70Y' },
-    { name: 'image19', url: 'https://ibb.co.com/275JPT2j' },
-    { name: 'image20', url: 'https://ibb.co.com/HT1HQmHf' },
-    { name: 'image21', url: 'https://ibb.co.com/KjJ7H5kR' },
-    { name: 'image22', url: 'https://ibb.co.com/zWJB87D2' },
-    { name: 'image23', url: 'https://ibb.co.com/yBMcm2Pw' },
-    { name: 'image24', url: 'https://ibb.co.com/fdN9M11f' },
-    { name: 'image25', url: 'https://ibb.co.com/GvTSvwfp' },
-    { name: 'image26', url: 'https://ibb.co.com/vxDv337q' },
-    { name: 'image27', url: 'https://ibb.co.com/hJpfKNGd' },
-    { name: 'image28', url: 'https://ibb.co.com/mF6z7hdc' },
-    { name: 'image29', url: 'https://ibb.co.com/HTqRY4QT' },
-    { name: 'image30', url: 'https://ibb.co.com/VYX847Cq' },
-    { name: 'image31', url: 'https://ibb.co.com/dshBSs70' },
-    { name: 'image32', url: 'https://ibb.co.com/JWnHLFGy' },
-    { name: 'image33', url: 'https://ibb.co.com/36ZLPfX' },
-    { name: 'image34', url: 'https://ibb.co.com/5Wy3sYBj' },
-    { name: 'image35', url: 'https://ibb.co.com/nNPdz2qy' },
-    { name: 'image36', url: 'https://ibb.co.com/LzWqjZtD' },
-    { name: 'image37', url: 'https://ibb.co.com/j97LQYHs' },
-    { name: 'image38', url: 'https://ibb.co.com/k2xmfN5H' },
-    { name: 'image39', url: 'https://ibb.co.com/7t1kb71t' },
-    { name: 'image40', url: 'https://ibb.co.com/Rkgd8hz5' },
-    { name: 'image41', url: 'https://ibb.co.com/spTq8JCs' },
-    { name: 'image42', url: 'https://ibb.co.com/qMppwDFB' },
-    { name: 'image43', url: 'https://ibb.co.com/pBVMLG4Y' },
-    { name: 'image44', url: 'https://ibb.co.com/ch2SkZ7t' },
-    { name: 'image45', url: 'https://ibb.co.com/pBZTTkc0' },
-    { name: 'image46', url: 'https://ibb.co.com/7x0cW047' },
-    { name: 'image47', url: 'https://ibb.co.com/Jw9v7FGr' },
-    { name: 'image48', url: 'https://ibb.co.com/wN7BvBj9' },
-    { name: 'image49', url: 'https://ibb.co.com/5g2XJyzt' },
-    { name: 'image50', url: 'https://ibb.co.com/931Rp46B' },
-    { name: 'image51', url: 'https://ibb.co.com/8DmRD6SK' },
-    { name: 'image52', url: 'https://ibb.co.com/S4VqSZQq' },
-    { name: 'image53', url: 'https://ibb.co.com/Kpd92t0q' },
-    { name: 'image54', url: 'https://ibb.co.com/8nGcmCYd' },
-    { name: 'image55', url: 'https://ibb.co.com/PvCw8zjL' },
-    { name: 'image56', url: 'https://ibb.co.com/6cbW1w5P' },
-    { name: 'image57', url: 'https://ibb.co.com/1tQHcv3p' },
-    { name: 'image58', url: 'https://ibb.co.com/RGkXgCSL' },
-    { name: 'image59', url: 'https://ibb.co.com/QLBZpsc' },
-    { name: 'image60', url: 'https://ibb.co.com/Q3h0sk5N' },
-    { name: 'image61', url: 'https://ibb.co.com/k2PbHDhc' },
-    { name: 'image62', url: 'https://ibb.co.com/XZ8cXY0N' },
-    { name: 'image63', url: 'https://ibb.co.com/MyG1Wvr4' },
-    { name: 'image64', url: 'https://ibb.co.com/4RZYFz3B' },
-    { name: 'image65', url: 'https://ibb.co.com/k28B9STD' },
-    { name: 'image66', url: 'https://ibb.co.com/xtsbFPQq' },
-    { name: 'image67', url: 'https://ibb.co.com/HpgNcLzj' },
-    { name: 'image68', url: 'https://ibb.co.com/5XZdkSLz' },
-    { name: 'image69', url: 'https://ibb.co.com/99Jm58Jb' },
-    { name: 'image70', url: 'https://ibb.co.com/DHhhK8Rz' },
-    { name: 'image71', url: 'https://ibb.co.com/Kjk7M7cN' },
-    { name: 'image72', url: 'https://ibb.co.com/RpJ8Q3nJ' },
-    { name: 'image73', url: 'https://ibb.co.com/cXcDczKd' },
-    { name: 'image74', url: 'https://ibb.co.com/cKp3ZqZz' },
-    { name: 'image75', url: 'https://ibb.co.com/hR4SP4tq' },
-    { name: 'image76', url: 'https://ibb.co.com/XfVK2xbK' },
-    { name: 'image77', url: 'https://ibb.co.com/CKPQDycj' },
-    { name: 'image78', url: 'https://ibb.co.com/CsmM5y4v' },
-    { name: 'image79', url: 'https://ibb.co.com/8DFTwS4h' },
-    { name: 'image80', url: 'https://ibb.co.com/93N624P7' },
+    {
+      name: "image2",
+      url: "https://i.ibb.co.com/KxhqV8tP/472441453-581409574654597-224275840110499575-n.jpg",
+    },
+    {
+      name: "image3",
+      url: "https://i.ibb.co.com/SX3vmPfD/472440864-581409641321257-4149183851865405067-n.jpg",
+    },
+    {
+      name: "image4",
+      url: "https://i.ibb.co.com/zVKz5sTw/472439815-581407384654816-1450345390000174245-n.jpg",
+    },
+    {
+      name: "image5",
+      url: "https://i.ibb.co.com/TDfvpLnM/472424160-581407631321458-7305782529008605775-n.jpg",
+    },
+    {
+      name: "image6",
+      url: "https://i.ibb.co.com/p61KVvq5/472405581-581885004607054-6198270127019096741-n.jpg",
+    },
+    {
+      name: "image7",
+      url: "https://i.ibb.co.com/dsBcQkXn/472398545-581407567988131-238927341627316883-n.jpg",
+    },
+    {
+      name: "image8",
+      url: "https://i.ibb.co.com/LDsC3j0d/472377041-581408611321360-8471131652011047860-n.jpg",
+    },
+    {
+      name: "image9",
+      url: "https://i.ibb.co.com/pjtVqrQn/472337525-581884847940403-23084244871058205-n.jpg",
+    },
+    {
+      name: "image10",
+      url: "https://i.ibb.co.com/N2n4h8kQ/472326982-581409467987941-2658842367746698535-n.jpg",
+    },
+    {
+      name: "image11",
+      url: "https://i.ibb.co.com/B2Q56FSP/472313559-581884724607082-476691951087786024-n.jpg",
+    },
+    {
+      name: "image12",
+      url: "https://i.ibb.co.com/HLwD5xLy/472287190-581407927988095-7281427138416045915-n.jpg",
+    },
+    {
+      name: "image13",
+      url: "https://i.ibb.co.com/GfRG66wQ/472277818-581885197940368-5580042814094858260-n.jpg",
+    },
+    {
+      name: "image14",
+      url: "https://i.ibb.co.com/LGnnCtz/472271897-581410084654546-6199225158863192378-n.jpg",
+    },
+    {
+      name: "image15",
+      url: "https://i.ibb.co.com/N2pm0Q9x/472268685-581407234654831-1530121384459924665-n.jpg",
+    },
+    {
+      name: "image16",
+      url: "https://i.ibb.co.com/vv11jRKs/472266832-581408477988040-1790819025689586688-n.jpg",
+    },
+    {
+      name: "image17",
+      url: "https://i.ibb.co.com/vCvs2fTD/472236166-581409607987927-1632847699180175047-n.jpg",
+    },
+    {
+      name: "image18",
+      url: "https://i.ibb.co.com/8g68Y0j4/472234674-581408124654742-3961741438832402493-n.jpg",
+    },
+    {
+      name: "image19",
+      url: "https://i.ibb.co.com/5W2ZvPD5/472234660-581409101321311-5301643412112326237-n.jpg",
+    },
+    {
+      name: "image20",
+      url: "https://i.ibb.co.com/LdbZf4ZX/472234592-581884974607057-1611085517043065961-n.jpg",
+    },
+    {
+      name: "image21",
+      url: "https://i.ibb.co.com/zhzbwmYj/472234431-581409037987984-2221541977763201236-n.jpg",
+    },
+    {
+      name: "image22",
+      url: "https://i.ibb.co.com/Cp2YJMD9/472233881-581409974654557-5426410010964389951-n.jpg",
+    },
+    {
+      name: "image23",
+      url: "https://i.ibb.co.com/fGKzVJFh/472233879-581885044607050-3964127984820330316-n.jpg",
+    },
+    {
+      name: "image24",
+      url: "https://i.ibb.co.com/hFdcs77b/472233782-581408244654730-2574902542027182560-n.jpg",
+    },
+    {
+      name: "image25",
+      url: "https://i.ibb.co.com/hRfzR5x9/472233761-581410151321206-7161464830048886085-n.jpg",
+    },
+    {
+      name: "image26",
+      url: "https://i.ibb.co.com/TBrq44V2/472233543-581409804654574-5065540681891351656-n.jpg",
+    },
+    {
+      name: "image27",
+      url: "https://i.ibb.co.com/S4qrKgkm/472233543-581407191321502-7118633076905374886-n.jpg",
+    },
+    {
+      name: "image28",
+      url: "https://i.ibb.co.com/HpngQGRH/472233374-581408521321369-8447581515117098962-n.jpg",
+    },
+    {
+      name: "image29",
+      url: "https://i.ibb.co.com/hFVrH9vF/472229101-581884794607075-7331277076705622754-n.jpg",
+    },
+    {
+      name: "image30",
+      url: "https://i.ibb.co.com/PGB8H7W6/472196326-581409281321293-4226703423634704845-n.jpg",
+    },
+    {
+      name: "image31",
+      url: "https://i.ibb.co.com/fVcMbVQY/472189577-581408684654686-1799197875113308160-n.jpg",
+    },
+    {
+      name: "image32",
+      url: "https://i.ibb.co.com/SDNBT7Md/472182995-581407147988173-2000780815016134866-n.jpg",
+    },
+    {
+      name: "image33",
+      url: "https://i.ibb.co.com/j0X1jVK/472182623-581410121321209-8893260691912175136-n.jpg",
+    },
+    {
+      name: "image34",
+      url: "https://i.ibb.co.com/MD4HP8M1/472181342-581410051321216-6331981278642257339-n.jpg",
+    },
+    {
+      name: "image35",
+      url: "https://i.ibb.co.com/dwmCG9sv/472179764-581408804654674-3425052201136058304-n.jpg",
+    },
+    {
+      name: "image36",
+      url: "https://i.ibb.co.com/3yq86CN5/472177318-581408204654734-915248634500907929-n.jpg",
+    },
+    {
+      name: "image37",
+      url: "https://i.ibb.co.com/gbsDf86h/472169335-581407341321487-6093428666973780314-n.jpg",
+    },
+    {
+      name: "image38",
+      url: "https://i.ibb.co.com/hFyYj3L7/472175720-581884927940395-208129760181034576-n.jpg",
+    },
+    {
+      name: "image39",
+      url: "https://i.ibb.co.com/1GmzbHmG/472166688-581407751321446-3594807660876659717-n.jpg",
+    },
+    {
+      name: "image40",
+      url: "https://i.ibb.co.com/JwcgZx5h/472165597-581408851321336-8418507516471483847-n.jpg",
+    },
+    {
+      name: "image41",
+      url: "https://i.ibb.co.com/vCNk0vhL/472151397-581407724654782-5974995235966699729-n.jpg",
+    },
+    {
+      name: "image42",
+      url: "https://i.ibb.co.com/BVBBRfKL/472143238-581407524654802-1190644002749789530-n.jpg",
+    },
+    {
+      name: "image43",
+      url: "https://i.ibb.co.com/gLqfRHtk/472143080-581407477988140-1094991683029577481-n.jpg",
+    },
+    {
+      name: "image44",
+      url: "https://i.ibb.co.com/d4kwjVz6/472140629-581408007988087-8388392628392984905-n.jpg",
+    },
+    {
+      name: "image45",
+      url: "https://i.ibb.co.com/MxpHHJr2/472130253-581409714654583-5175917990709482082-n.jpg",
+    },
+    {
+      name: "image46",
+      url: "https://i.ibb.co.com/RGJn0JyL/472122774-581409151321306-63451269562385008-n.jpg",
+    },
+    {
+      name: "image47",
+      url: "https://i.ibb.co.com/8LHr54fP/472105659-581408947987993-7691413805147479037-n.jpg",
+    },
+    {
+      name: "image48",
+      url: "https://i.ibb.co.com/RpNY5Ydx/472105414-581409191321302-1914184852851288190-n.jpg",
+    },
+    {
+      name: "image49",
+      url: "https://i.ibb.co.com/Rk7GJmMZ/472104847-581408441321377-1967475783815531464-n.jpg",
+    },
+    {
+      name: "image50",
+      url: "https://i.ibb.co.com/jZQ78wmq/472102388-581408371321384-4444167321630518720-n.jpg",
+    },
+    {
+      name: "image51",
+      url: "https://i.ibb.co.com/SX61XR9s/472097002-581410554654499-1897780764570657222-n.jpg",
+    },
+    {
+      name: "image52",
+      url: "https://i.ibb.co.com/W4BSCRDS/472075426-581407421321479-6449024338050465640-n.jpg",
+    },
+    {
+      name: "image53",
+      url: "https://i.ibb.co.com/Nd5L90Vj/472074781-581409754654579-7126035052042462478-n.jpg",
+    },
+    {
+      name: "image54",
+      url: "https://i.ibb.co.com/4wCdFq1Y/472073167-581408161321405-8483146036920122296-n.jpg",
+    },
+    {
+      name: "image55",
+      url: "https://i.ibb.co.com/kscQF6B7/472066969-581408044654750-7910212470360597049-n.jpg",
+    },
+    {
+      name: "image56",
+      url: "https://i.ibb.co.com/xKG7fLQj/472055798-581410201321201-1685532255372548367-n.jpg",
+    },
+    {
+      name: "image57",
+      url: "https://i.ibb.co.com/JwtXZBNb/472053888-581408761321345-4822622355229678845-n.jpg",
+    },
+    {
+      name: "image58",
+      url: "https://i.ibb.co.com/5XgQ159q/472053636-581408714654683-1282430401497630227-n.jpg",
+    },
+    {
+      name: "image59",
+      url: "https://i.ibb.co.com/BbDmc8P/472038261-581884761273745-7482899712604335389-n.jpg",
+    },
+    {
+      name: "image60",
+      url: "https://i.ibb.co.com/hJr0pcqW/472024091-581409527987935-8255929459429190386-n.jpg",
+    },
+    {
+      name: "image61",
+      url: "https://i.ibb.co.com/WvR8xzcK/472010444-581408404654714-8661912960065424816-n.jpg",
+    },
+    {
+      name: "image62",
+      url: "https://i.ibb.co.com/vxY01Xyg/472004786-581408994654655-669436573701693297-n.jpg",
+    },
+    {
+      name: "image63",
+      url: "https://i.ibb.co.com/fVnNBgf5/472000453-581409497987938-1714313125546936692-n.jpg",
+    },
+    {
+      name: "image64",
+      url: "https://i.ibb.co.com/xqSf2WdV/471999692-581407807988107-5420077643223062324-n.jpg",
+    },
+    {
+      name: "image65",
+      url: "https://i.ibb.co.com/Wvy5P2Xz/471996512-581410517987836-6172616844138516679-n.jpg",
+    },
+    {
+      name: "image66",
+      url: "https://i.ibb.co.com/cSv9YMdh/471991611-581407894654765-8790630381842036196-n.jpg",
+    },
+    {
+      name: "image67",
+      url: "https://i.ibb.co.com/LhpQTDt3/471986766-581407284654826-2920408710126091203-n.jpg",
+    },
+    {
+      name: "image68",
+      url: "https://i.ibb.co.com/8nkJbf9R/471982345-581411494654405-4787227132168041264-n.jpg",
+    },
+    {
+      name: "image69",
+      url: "https://i.ibb.co.com/SX2DCs2t/471982133-581408337988054-1730886668791816719-n.jpg",
+    },
+    {
+      name: "image70",
+      url: "https://i.ibb.co.com/21xxKskW/471951298-581407967988091-5042644234918438888-n.jpg",
+    },
+    {
+      name: "image71",
+      url: "https://i.ibb.co.com/4ZGM6Mws/471951164-581408637988024-7300819202405373361-n.jpg",
+    },
+    {
+      name: "image72",
+      url: "https://i.ibb.co.com/MDJdPcTJ/471950543-581407684654786-3386105287795957602-n.jpg",
+    },
+    {
+      name: "image73",
+      url: "https://i.ibb.co.com/7NJ2J7xF/471950108-581408564654698-5099025383163825874-n.jpg",
+    },
+    {
+      name: "image74",
+      url: "https://i.ibb.co.com/SD1yMSMq/471950013-581408891321332-7957613524068368329-n.jpg",
+    },
+    {
+      name: "image75",
+      url: "https://i.ibb.co.com/4ZXH6X5Q/471949938-581409381321283-3911492005232098681-n.jpg",
+    },
+    {
+      name: "image76",
+      url: "https://i.ibb.co.com/BHwQz2tQ/471949934-581407104654844-1164426646739085714-n.jpg",
+    },
+    {
+      name: "image77",
+      url: "https://i.ibb.co.com/YB0hwSC5/471949146-581409681321253-8656768079417917903-n.jpg",
+    },
+    {
+      name: "image78",
+      url: "https://i.ibb.co.com/Z1GY6rvK/471949914-581408287988059-4139336551912443825-n.jpg",
+    },
+    {
+      name: "image79",
+      url: "https://i.ibb.co.com/rGnTh1fW/471946519-581409417987946-8639062323813832980-n.jpg",
+    },
+    {
+      name: "image80",
+      url: "https://i.ibb.co.com/Nns5pNRM/471941142-581408081321413-311395801393515545-n.jpg",
+    },
   ],
 
   reviews: [
